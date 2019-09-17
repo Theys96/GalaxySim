f423417c0c758101870563beabec9cc8b700caaa
Theys96 <thijs@thijshavinga.nl>
2019-09-17 10:20:40 +0200

    Small changes

--- c/nbody.c
+++ c/nbody.c
@@ -5,13 +5,13 @@
 #include "bodies.h"
 #include "image.h"
 
-#define DT 0.0005
+#define DT 0.05
 #define PI 3.141592
 
 int main(int argc, char** argv) {
 	srand(time(NULL));
 
-	int N = 4000;
+	int N = 2;
 	Body bodies[N];
 
 	//bodies[0] = newBody(-30, 0, 0, 0, 3, 0, 1);
@@ -19,16 +19,19 @@ int main(int argc, char** argv) {
 	//bodies[2] = newBody(0, -30, 0, -3, 0, 0, 1);
 	//bodies[3] = newBody(0, 30, 0, 3, 0, 0, 1);
 	//bodies[4] = newBody(0, 0, 0, 0, 0, 0, 100);
-	double theta, r, rx, ry, gapSize = 35;
-	bodies[0] = newBody(0, 10, 0, -2, 0, 0, 100*N);
-	bodies[1] = newBody(0, -10, 0, 2, 0, 0, 100*N);
-	for (int i = 2; i < N; i++) {
+	bodies[0] = newBody(0, 0, 0, 0, 0, 0, 100000);
+	bodies[1] = newBody(-100, 0, 0, 0, -sqrt(100001 * 1/100), 0, 1);
+
+	/*
+	double theta, r, rx, ry, gapSize = 20;
+	for (int i = 2; i < 600; i++) {
 		theta = PI * (rand() % 360) / 180;
-		r = (rand() % 250);
-		rx = gapSize + r * 1.0;
+		r = rand() % (100 - (int)gapSize);
+		rx = gapSize + r * .5;
 		ry = gapSize + r * 1.0;
-		bodies[i] = newBody(rx*cos(theta)*0.3, 1+ry*sin(theta), 0, -2*pow(80/rx,0.5)*sin(theta), 2*pow(80/ry,0.5)*cos(theta), 0, 10);
+		bodies[i] = newBody(rx*cos(theta), 1+ry*sin(theta), 0, -2*pow(100/rx,0.5)*sin(theta), 2*pow(100/ry,0.5)*cos(theta), 0, 100);
 	}
+	*/
 	
 	int width = 400;
 	int height = 400;
@@ -40,29 +43,36 @@ int main(int argc, char** argv) {
 	int nframes = 1000;
 
 	int percent = nframes/100;
-	for (int i = 0; i < nframes; i++) {
-		if (i % percent == 0) {
-			printf("%d%%\n", i/percent);
+
+	Image img = newImage(width, height);
+
+	for (int frame = 0; frame < nframes; frame++) {
+		if (frame % percent == 0) {
+			printf("%d%%\n", frame/percent);
 		}
 
-		Image img = newImage(width, height);
 
 		//cx = width/2 - (int)bodies[0].x;
 		//cy = height/2 - (int)bodies[0].y;
 
 		Body a;
-		for (int i = 0; i < N; i++) {
-			a = bodies[i];
-			if ( (int)(a.x+cx) >= 0 && (int)(a.x+cx) < width && (int)(a.y+cy) >= 0 && (int)(a.y+cy) < height) {
-				img.img[(int)a.y+cy][(int)a.x+cx] = 255;
+		if (frame > nframes/10*9 || frame%1 == 0) {
+
+			
+
+			for (int i = 0; i < N; i++) {
+				a = bodies[i];
+				if ( (int)(a.x+cx) >= 0 && (int)(a.x+cx) < width && (int)(a.y+cy) >= 0 && (int)(a.y+cy) < height) {
+					img.img[(int)a.y+cy][(int)a.x+cx] = a.mass > 100 ? 255 : 255;
+					
+				}
 			}
+
+			sprintf(filename, "image%04d.pgm", frame+1);
+			saveImage(img, filename);
 		}
 
-		sprintf(filename, "image%04d.pgm", i+1);
-		saveImage(img, filename);
-		freeImage(img);
-
-		// Update locations
+		// Update velocities
 		for (int i = 0; i < N; i++) {
 			Body *b, *c;
 			b = &bodies[i];
@@ -77,13 +87,15 @@ int main(int argc, char** argv) {
 				c->dz -= DT * f[2]/(c->mass);
 			}
 		}
-		
-		// #pragma omp parallel for
+
+		// Update locations
 		for (int i = 0; i < N; i++) {
 			Body* b = &bodies[i];
-			b->x += b->dx;
-			b->y += b->dy;
-			b->z += b->dz;
+			b->x += DT * b->dx;
+			b->y += DT * b->dy;
+			b->z += DT * b->dz;
 		}
 	}
+
+			freeImage(img);
 }
