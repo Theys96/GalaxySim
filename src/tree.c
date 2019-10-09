d39509752f7ae5e55ad6652ed7e434ad88eeedfa
Theys96 <thijs@thijshavinga.nl>
2019-10-09 15:40:11 +0200

    Finished refactoring Barnes Hut

--- src/tree.c
+++ src/tree.c
@@ -12,19 +12,19 @@
 #include <stdio.h>
 #include <time.h>
 
+/* LOCAL FUNCTIONS*/
+Subnode newSubnode(UniverseSize universe_size);
+void freeSubnode(Subnode *s);
+UniverseSize getUniverseSize(Universe u);
+UniverseSize getQuadrantSize(UniverseSize original, int index);
+Subnode *getQuadrant(Subnode *s, Body body);
+void insertBody(Subnode *s, Body body);
+void computeMass(Subnode *s);
+double max(double a, double b, double c);
+
 #define DELTA_STAR 0.0001
 #define DELTA_BARNES_HUT 0.5
 
-Subnode newSubnode(UniverseSize universe_size) {
-  Subnode s;
-  for (size_t i = 0; i < 8; i++) {
-    s.children[i] = NULL;
-  }
-  s.value = newBody(0,0,0,0,0,0,0);
-  s.universe_size = universe_size;
-  s.node_count = 0;
-  return s;
-}
 
 Tree newTree(Universe u) {
   Tree t = calloc(1, sizeof(Subnode));
@@ -39,6 +39,47 @@ Tree newTree(Universe u) {
   return t;
 }
 
+void computeForceFromTree(Body object, Subnode *s, double fvec[3]) {
+  fvec[0] = 0; fvec[1] = 0; fvec[2] = 0; 
+  if (s->node_count == 1) {
+    computeForce(object, s->value, fvec);
+  } else {
+    double radius = bodyDistance(object, s->value);
+    double height = max(s->size.max_x - s->size.min_x, s->size.max_y - s->size.min_y, s->size.max_z - s->size.min_z);
+    if (height/radius < DELTA_BARNES_HUT) {
+      computeForce(object, s->value, fvec);
+    } else {
+      // compute the forces for all child cubic cells
+      for (size_t i = 0; i < 8; i++) {
+        if (s->children[i] != NULL) {
+          double fsubvec[3];
+          computeForceFromTree(object, s->children[i], fsubvec);
+          fvec[0] += fsubvec[0];
+          fvec[1] += fsubvec[1];
+          fvec[2] += fsubvec[2];
+        }
+      }
+    }
+  }
+}
+
+void freeTree(Tree t) {
+  freeSubnode(t);
+}
+
+
+
+Subnode newSubnode(UniverseSize universe_size) {
+  Subnode s;
+  for (size_t i = 0; i < 8; i++) {
+    s.children[i] = NULL;
+  }
+  s.value = newBody(0,0,0,0,0,0,0);
+  s.size = universe_size;
+  s.node_count = 0;
+  return s;
+}
+
 void freeSubnode(Subnode *s) {
   for (size_t i = 0; i < 8; i++) {
     if (s->children[i] != NULL) {
@@ -46,10 +87,6 @@ void freeSubnode(Subnode *s) {
     }
   }
   free(s);
-}
-
-void freeTree(Tree t) {
-  freeSubnode(t);
 }
 
 void insertBody(Subnode *s, Body body) {
@@ -77,32 +114,24 @@ void insertBody(Subnode *s, Body body) {
 }
 
 UniverseSize getUniverseSize(Universe u) {
-  UniverseSize universe_size;
-  universe_size.min_x = u.bodies[0].x;
-  universe_size.max_x = u.bodies[0].x;
-  universe_size.min_y = u.bodies[0].y;
-  universe_size.max_y = u.bodies[0].y;
-  universe_size.min_z = u.bodies[0].z;
-  universe_size.max_z = u.bodies[0].z;
+  UniverseSize sz;
+
+  sz.min_x = u.bodies[0].x;
+  sz.max_x = u.bodies[0].x;
+  sz.min_y = u.bodies[0].y;
+  sz.max_y = u.bodies[0].y;
+  sz.min_z = u.bodies[0].z;
+  sz.max_z = u.bodies[0].z;
   for (int i = 1; i < u.n; i++) {
-    if (u.bodies[i].x < universe_size.min_x) {
-      universe_size.min_x = u.bodies[i].x;
-    } else if (u.bodies[i].x > universe_size.max_x) {
-      universe_size.max_x = u.bodies[i].x;
-    }
-    if (u.bodies[i].y < universe_size.min_y) {
-      universe_size.min_y = u.bodies[i].y;
-    } else if (u.bodies[i].y > universe_size.max_y) {
-      universe_size.max_y = u.bodies[i].y;
-    }
-    if (u.bodies[i].z < universe_size.min_z) {
-      universe_size.min_z = u.bodies[i].z;
-    } else if (u.bodies[i].z > universe_size.max_z) {
-      universe_size.max_z = u.bodies[i].z;
-    }
+    sz.min_x = u.bodies[i].x < sz.min_x ? u.bodies[i].x : sz.min_x;
+    sz.max_x = u.bodies[i].x > sz.max_x ? u.bodies[i].x : sz.max_x;
+    sz.min_y = u.bodies[i].y < sz.min_y ? u.bodies[i].y : sz.min_y;
+    sz.max_y = u.bodies[i].y > sz.max_y ? u.bodies[i].y : sz.max_y;
+    sz.min_z = u.bodies[i].z < sz.min_z ? u.bodies[i].z : sz.min_z;
+    sz.max_z = u.bodies[i].z > sz.max_z ? u.bodies[i].z : sz.max_z;
   }
 
-  return universe_size;
+  return sz;
 }
 
 UniverseSize getQuadrantSize(UniverseSize original, int index) {
@@ -126,37 +155,35 @@ UniverseSize getQuadrantSize(UniverseSize original, int index) {
 }
 
 Subnode *getQuadrant(Subnode *s, Body body) {
-  UniverseSize universe_size = s->universe_size;
+  UniverseSize sz = s->size;
   int index = 
-    1 * (body.x > (universe_size.max_x + universe_size.min_x) / 2.0) +
-    2 * (body.y > (universe_size.max_y + universe_size.min_y) / 2.0) + 
-    4 * (body.z > (universe_size.max_z + universe_size.min_z) / 2.0);
+    1 * (body.x > (sz.max_x + sz.min_x) / 2.0) +
+    2 * (body.y > (sz.max_y + sz.min_y) / 2.0) + 
+    4 * (body.z > (sz.max_z + sz.min_z) / 2.0);
 
   if (s->children[index] == NULL) {
     s->children[index] = calloc(1, sizeof(Subnode));
-    *(s->children[index]) = newSubnode(getQuadrantSize(universe_size, index));
+    *(s->children[index]) = newSubnode(getQuadrantSize(sz, index));
   }
 
   return s->children[index];
 }
 
 void computeMass(Subnode *s) {
+  // Only for nodes with more than 1 body an operation is required
   if (s->node_count > 1) {
     s->value = newBody(0,0,0,0,0,0,0);
-    // calculate the mass dependent on all child cubic cells
     for (size_t i = 0; i < 8; i++) {
       if (s->children[i] != NULL) {
         computeMass(s->children[i]);
-        s->value.mass += s->children[i]->value.mass;
-        s->value.x += s->children[i]->value.x;
-        s->value.y += s->children[i]->value.y;
-        s->value.z += s->children[i]->value.z;
+        Body com = s->children[i]->value;
+        if (com.mass > 0.0) {
+          s->value.mass += com.mass;
+          s->value.x += com.x / com.mass;
+          s->value.y += com.y / com.mass;
+          s->value.z += com.z / com.mass;
+        }
       }
-    }
-    if (s->value.mass > 0.0) {
-      s->value.x /= s->value.mass;
-      s->value.y /= s->value.mass;
-      s->value.z /= s->value.mass;
     }
   }
 }
@@ -165,28 +192,4 @@ double max(double a, double b, double c) {
   double result = (a > b) ? a : b;
   result = (result > c) ? result : c;
   return result;
-}
-
-void computeForceFromTree(Body object, Subnode *s, double fvec[3]) {
-  fvec[0] = 0; fvec[1] = 0; fvec[2] = 0; 
-  if (s->node_count == 1) {
-    computeForce(object, s->value, fvec);
-  } else {
-    double radius = bodyDistance(object, s->value);
-    double height = max(s->universe_size.max_x - s->universe_size.min_x, s->universe_size.max_y - s->universe_size.min_y, s->universe_size.max_z - s->universe_size.min_z);
-    if (height/radius < DELTA_BARNES_HUT) {
-      computeForce(object, s->value, fvec);
-    } else {
-      // compute the forces for all child cubic cells
-      for (size_t i = 0; i < 8; i++) {
-        if (s->children[i] != NULL) {
-          double fsubvec[3];
-          computeForceFromTree(object, s->children[i], fsubvec);
-          fvec[0] += fsubvec[0];
-          fvec[1] += fsubvec[1];
-          fvec[2] += fsubvec[2];
-        }
-      }
-    }
-  }
 }
--- src/tree.h
+++ src/tree.h
@@ -24,24 +24,14 @@ typedef struct Subnode Subnode;
 struct Subnode {
   Subnode* children[8];
   Body value;
-  UniverseSize universe_size;
+  UniverseSize size;
   int node_count;
 };
 
 typedef Subnode* Tree;
 
-Subnode newSubnode(UniverseSize universe_size);
 Tree newTree(Universe u);
-void freeSubnode(Subnode *s);
-void freeTree(Tree t);
-
-void insertBody(Subnode *s, Body body);
-UniverseSize getUniverseSize(Universe u);
-UniverseSize getQuadrantSize(UniverseSize original, int index);
-Subnode *getQuadrant(Subnode *s, Body body);
-
-void computeMass(Subnode *s);
-double max(double a, double b, double c);
 void computeForceFromTree(Body object, Subnode *s, double fvec[3]);
+void freeTree(Tree t);
 
 #endif
