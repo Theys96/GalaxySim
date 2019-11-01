60ba79bc7cb73c708e8a704f1aa129d3e3d45e7d
Theys96 <thijs@thijshavinga.nl>
2019-11-01 19:10:36 +0100

    Did analysis on spiral galaxies

--- src/main.c
+++ src/main.c
@@ -8,14 +8,15 @@
 #define str(x) #x
 #define xstr(x) str(x)
 
-#define CURRENT_MAIN defaultMain
-#define VERSION "v1.2.5"
+#define CURRENT_MAIN customMain_spiralGalaxies
+#define VERSION "v1.2.6"
 
 /* PARAMETERS */
 int n = 1000;
 int size = 400;
 int nframes = 500;
 int r = 250;
+int scale = 1;
 double dt = 0.02;
 double theta = 0.5;
 typedef enum { Euler = 0, BarnesHut = 1, MostSignificant = 2 } Method;
@@ -25,6 +26,7 @@ Method method = Euler;
 int defaultMain(int argc, char** argv);
 int customMain_energies(int argc, char** argv);
 int customMain_collidingGalaxies(int argc, char** argv);
+int customMain_spiralGalaxies(int argc, char** argv);
 
 // Helper functions:
 void printTimestamp();
@@ -50,18 +52,13 @@ int defaultMain(int argc, char** argv) {
   printf("\nSpecifications:\n");
   printf("\tn                 = %d\n", n);
   printf("\timage dimensions  = %dx%d\n", size, size);
+  printf("\tscale             = %d\n", scale);
   printf("\tframes            = %d\n", nframes);
   printf("\titeration method  = %s\n", methodString(method));
   if (method == BarnesHut) {
     printf("\tBarnes-Hut theta  = %.3f\n", theta);
   }
   printf("\n");
-
-  //Universe b      = newSingleBodyUniverse(newBody(0,0,0,0,0,0,100000));
-  //Universe spiral = newSpiralUniverse(n, r, 30);
-  //Universe uni    = combineUniverses(spiral, newBody(0,0,0,0,0,0,0), b, newBody(0,-(1.5*r),0,sqrt((100000+1000*n)/(1.5*r)),0,0,0));
-  //freeUniverse(b);
-  //freeUniverse(spiral);
 
   Universe uni = newCircularUniverse(n, r, 30);
   // universeToCsv(uni, "state0.csv");
@@ -76,7 +73,7 @@ int defaultMain(int argc, char** argv) {
     sprintf(filename, "img/image%04d.pbm", i+1);
 
     t0 = clock();
-    renderUniverse(uni, filename, 0, 0, 1, size, size, 0);
+    renderUniverse(uni, filename, 0, 0, scale, size, size, 0);
     t1 = clock();
     timeRendering += (double)(t1 - t0) / CLOCKS_PER_SEC;
 
@@ -124,6 +121,97 @@ int defaultMain(int argc, char** argv) {
 
 
 /* CUSTOM MAIN FUNCTIONS (FOR SPECIFIC EXPERIMENTS) */
+int customMain_spiralGalaxies(int argc, char** argv) {
+
+  scale = 5;
+  r = 750;
+  n = 3000;
+  //size = 250;
+  readParameters();
+
+  printTitle();
+
+  printf("\nSpecifications:\n");
+  printf("\tn                 = %d\n", n);
+  printf("\timage dimensions  = %dx%d\n", size, size);
+  printf("\tscale             = %d\n", scale);
+  printf("\tframes            = %d\n", nframes);
+  printf("\titeration method  = %s\n", methodString(method));
+  if (method == BarnesHut) {
+    printf("\tBarnes-Hut theta  = %.3f\n", theta);
+  }
+  printf("\n");
+
+  Universe b      = newSingleBodyUniverse(newBody(0,0,0,0,0,0,20*n));
+  Universe spiral = newSpiralUniverse(n, r, 30);
+  Universe uni0   = combineUniverses(spiral, newBody(0,0,0,0,0,0,0), b, newBody((2*r),0,0,0,sqrt((1020*n)/(2*r)),0,0));
+  Universe uni    = combineUniverses(uni0, newBody(0,0,0,0,0,0,0), b, newBody(-(2*r),0,0,0,-sqrt((1020*n)/(2*r)),0,0));
+  freeUniverse(b);
+  freeUniverse(spiral);
+  freeUniverse(uni0);
+
+  //Universe uni = newSpiralUniverse(n, r, 30);
+
+  double energy0 = totalEnergy(uni);
+
+  char filename[30], csv_filename[30];
+  clock_t t0, t1, total0, total1;
+  double timeRendering = 0, timeComputing = 0;
+  total0 = clock();
+  for (int i = 0; i < nframes; i++) {
+    sprintf(filename, "img/image%04d.pbm", i+1);
+
+    t0 = clock();
+    renderUniverse(uni, filename, 0, 0, scale, size, size, 0);
+    t1 = clock();
+    timeRendering += (double)(t1 - t0) / CLOCKS_PER_SEC;
+
+    if (i == 0 || (i+1)%100 == 0) {
+      sprintf(csv_filename, "img/csv_state%04d.csv", i+1);
+      universeToCsv(uni, csv_filename);
+    }
+
+    t0 = clock();
+    switch (method) {
+      default:
+      case Euler:
+        iterateEuler(&uni, dt);
+        break;
+
+      case BarnesHut:
+        iterateBarnesHut(&uni, dt, theta);
+        break;
+
+      case MostSignificant:
+        iterateMostSignificant(&uni, dt, 1000);
+        break;
+    }
+    t1 = clock();
+    timeComputing += (double)(t1 - t0) / CLOCKS_PER_SEC;
+  }
+  total1 = clock();
+
+  freeUniverse(uni);
+
+  double energy1 = totalEnergy(uni);
+  double energyDiff = energy1 - energy0;
+
+  printf("Done.\n");
+  printf("%s %.2lf%% of the universe's energy during this simulation.\n", energyDiff > 0 ? "Gained" : "Lost", fabs(energyDiff)/energy0*100);
+  printf("\n");
+
+  printf("Time used: %.3fs\n\n", (double)(total1 - total0) / CLOCKS_PER_SEC );
+  printf("Time used rendering images and saving them to file:\n");
+  printf("\t%.3fs total for %d frames\n", timeRendering, nframes);
+  printf("\t%.3fms on average\n", timeRendering / nframes * 1000 );
+  printf("\n");
+  printf("Time used updating the universe state:\n");
+  printf("\t%.3fs total for %d frames\n", timeComputing, nframes);
+  printf("\t%.3fms on average\n", timeComputing / nframes * 1000 );
+  printf("\n");
+
+  return 0;
+}
 
 int customMain_collidingGalaxies(int argc, char** argv) {
 
@@ -289,6 +377,7 @@ void readParameters() {
   readIntegerParameter("Number of bodies", &n);
   readIntegerParameter("Galaxy radius", &r);
   readIntegerParameter("Image size", &size);
+  readIntegerParameter("Image scale", &scale);
   readIntegerParameter("Number of frames", &nframes);
 
   getchar();
